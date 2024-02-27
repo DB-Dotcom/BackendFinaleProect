26d8b937d3a7d64851e3728caa676161d4ddf11b
WebDevDB <db.dci.student@gmail.com>
2024-02-27 22:53:02 +0100

    clear code from tests

--- test/user.test.js
+++ test/user.test.js
@@ -1,11 +1,1 @@
 // Importieren der benötigten Module
-import request from 'supertest';
-import app from './app';
-
-describe('GET /', () => {
-  it('sollte den Statuscode 200 und eine Willkommensnachricht zurückgeben', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toContain('Willkommen');
-  });
-});
